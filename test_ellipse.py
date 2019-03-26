049b44b2b091eb819c837386828bd9c243885764
Libao Jin <jinlibao@outlook.com>
2019-03-26 14:30:02 -0600

    Add plots for ellipses from Tweets data

--- test_ellipse.py
+++ test_ellipse.py
@@ -24,6 +24,22 @@ def test_ellipse():
     print(au.are_two_ellipses_overlapping(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2))
     au.plot_ellipses(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2)
 
+    file_open = fu.FileOpen("data", "tweet-data.csv")
+    tweet_spatial_analysis_config = cu.TweetSpatialAnalysisConfig("conf/tweet_spatial_analysis.ini")
+    tdp = TweetDataPreProcessing(file_open, tweet_spatial_analysis_config)
+    tdp.read_from_json("data/tweet_mean_all.json",
+                       "data/tweets_median_working.json",
+                       "data/tweets_median_non_working.json")
+    df = tdp.tweet_data_working.df
+    i = 0
+    j = 5
+    x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2 = df['x'][i], df['y'][i], df['a'][i], df['b'][i], df['angle'][i], \
+        df['x'][j], df['y'][j], df['a'][j], df['b'][j], df['angle'][j]
+    if max(a1, b1) > max(a2, b2):
+        x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2, i, j = x2, y2, a2, b2, phi2, x1, y1, a1, b1, phi1, j, i
+    print(au.are_two_ellipses_overlapping(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2))
+    au.plot_ellipses(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2, '{:d}_{:d}.pdf'.format(i, j))
+
 
 if __name__ == '__main__':
     test_ellipse()
