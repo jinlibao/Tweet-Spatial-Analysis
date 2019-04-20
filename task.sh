9ad7f6e293883c9a8b2f19f9eb0dd6491dbcd655
Libao Jin <jinlibao@outlook.com>
2019-04-19 23:28:15 -0600

    Add options for Intel MPI and OpenMPI

--- task.sh
+++ task.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=/gscratch/ljin1/data/twitter/log/overlap.%j.out
 #SBATCH --error=/gscratch/ljin1/data/twitter/log/overlap.%j.err
 #SBATCH --mail-type=END,FAIL
-### #SBATCH --mail-type=NONE
+## SBATCH --mail-type=NONE
 #SBATCH --mail-user=ljin1@uwyo.edu
 #SBATCH --time=6-23:59:59
 
@@ -24,4 +24,18 @@ if [ ! -d $DATA_DIR ]; then
 fi
 
 echo $NCPU
-mpirun -n $NCPU python3 ./build_overlap_matrix.py -l $DATA_DIR -r 50
+module purge -q
+module load arcc/0.1 slurm/18.08 swset/2018.05
+if [[ $MPI_VENDOR == "intel" ]]; then
+    echo "Intel MPI"
+    module load intel/18.0.1 intel-mpi/2018.2.199 intel-mkl/2018.2.199
+    srun python3 ./build_overlap_matrix.py -l $DATA_DIR
+    #srun python3 ./build_overlap_matrix.py -l $DATA_DIR -r 1000
+else
+    echo "Open MPI"
+    module load gcc/7.3.0 openmpi/3.1.0 intel-mkl/2018.2.199
+    mpirun -np $NCPU python3 ./build_overlap_matrix.py -l $DATA_DIR
+    #mpirun -np $NCPU python3 ./build_overlap_matrix.py -l $DATA_DIR -r 1000
+fi
+
+which python3
