2b8437efdc5306105e649b65acee6fdc206881b6
Libao Jin <jinlibao@outlook.com>
2019-04-23 12:11:43 -0600

    Update test_ellipse.py

--- test_ellipse.py
+++ test_ellipse.py
@@ -17,29 +17,16 @@ from data_preprocessing import TweetDataPreProcessing
 import build_overlap_matrix as bom
 
 def test_ellipse():
-    # x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2 = 0, 0, 2, 1, 0, 0, 0, 3, 2, 0
-    # x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2 = 2, 1, 2, 1, 0, -1, 2, 3, 2, 0.5
-    # x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2 = 2, 1, 2, 1, 0.5, -1, 2, 3, 1.5, 0.5
-    x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2 = 2, 1, 2, 1, 0.5, -1, 2, 3, 1.5, 1.6
-    print(au.are_two_ellipses_overlapping(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2))
-    au.plot_ellipses(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2)
-
-    file_open = fu.FileOpen("data", "tweet-data.csv")
-    tweet_spatial_analysis_config = cu.TweetSpatialAnalysisConfig("conf/tweet_spatial_analysis.ini")
-    tdp = TweetDataPreProcessing(file_open, tweet_spatial_analysis_config)
-    tdp.read_from_json("data/tweet_mean_all.json",
-                       "data/tweets_median_working.json",
-                       "data/tweets_median_non_working.json")
-    df = tdp.tweet_data_working.df
-    i = 0
-    j = 5
-    x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2 = df['x'][i], df['y'][i], df['a'][i], df['b'][i], df['angle'][i], \
-        df['x'][j], df['y'][j], df['a'][j], df['b'][j], df['angle'][j]
-    if max(a1, b1) > max(a2, b2):
-        x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2, i, j = x2, y2, a2, b2, phi2, x1, y1, a1, b1, phi1, j, i
-    print(au.are_two_ellipses_overlapping(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2))
-    au.plot_ellipses(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2, '{:d}_{:d}.pdf'.format(i, j))
-
+    A = [
+        [0, 0, 2, 1, 0, 0, 0, 3, 2, 0],
+        [2, 1, 2, 1, 0, -1, 2, 3, 2, 0.5],
+        [2, 1, 2, 1, 0.5, -1, 2, 3, 1.5, 0.5],
+        [2, 1, 2, 1, 0.5, -1, 2, 3, 1.5, 1.6]
+    ]
+    for i in range(len(A)):
+        x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2 = A[i]
+        print(au.are_two_ellipses_overlapping(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2))
+        au.plot_ellipses(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2)
 
 if __name__ == '__main__':
     test_ellipse()
