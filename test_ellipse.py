139eaec80e25e8837ae1fbf848b8142c807ff88f
Libao Jin <jinlibao@outlook.com>
2019-04-23 13:48:02 -0600

    Add generate_csv in test_ellipse to filter data

--- test_ellipse.py
+++ test_ellipse.py
@@ -28,5 +28,22 @@ def test_ellipse():
         print(au.are_two_ellipses_overlapping(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2))
         au.plot_ellipses(x1, y1, a1, b1, phi1, x2, y2, a2, b2, phi2)
 
+def generate_csv():
+    file_open = fu.FileOpen("data", "tweet-data.csv")
+    tweet_spatial_analysis_config = cu.TweetSpatialAnalysisConfig("conf/tweet_spatial_analysis.ini")
+    tdp = TweetDataPreProcessing(file_open, tweet_spatial_analysis_config)
+    tdp.read_from_json("data/tweet_mean_all.json",
+                       "data/tweets_median_working.json",
+                       "data/tweets_median_non_working.json")
+    df = []
+    df.append(tdp.tweet_data_working.df[['x', 'y', 'a', 'b', 'angle', 'id']])
+    df.append(tdp.tweet_data_non_working.df[['x', 'y', 'a', 'b', 'angle', 'id']])
+    df.append(tdp.tweet_data_all.df[['x', 'y', 'a', 'b', 'angle', 'id']])
+    filenames = ['data/tweets_median_working_filtered.csv', 'data/tweets_median_non_working_filtered.csv', 'data/tweets_mean_all_filtered.csv' ]
+    for i in range(len(df)):
+        df[i].to_csv(filenames[i], sep=',', header=False, index=False)
+
+
 if __name__ == '__main__':
-    test_ellipse()
+    # test_ellipse()
+    generate_csv()
